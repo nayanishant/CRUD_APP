a5ee136d7a79411dfcb556d265a81d7385faad95
NishantSwat <nayanishant.info@gmail.com>
2024-02-02 18:21:30 +0530

    added frontend

---
+++ Login_Functionality/frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import axios from "axios";
+import "./Login.css";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async () => {
+    const data = {
+      email,
+      username,
+      password,
+    };
+
+    try {
+      await axios.post("http://localhost:5000/login", data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="card">
+        <div className="cardHeader">
+          <h1>Welcome</h1>
+        </div>
+        <div className="cardBody">
+          <div>
+            <input
+              type="text"
+              placeholder="Type your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Type your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Type your password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="cardFooter">
+          <button onClick={handleSubmit}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
